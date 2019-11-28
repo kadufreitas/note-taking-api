c6c8e86cfcd3321f08c76cc4ef00ac830f65df94
kadufreitas <kadufreitas95@gmail.com>
2019-11-28 08:08:24 -0300

    reajustes

--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django_extensions',
     'rest_framework',
     'drf_yasg',
     'rest_framework.authtoken',
