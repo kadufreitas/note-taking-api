a48b8345f510f378118a62e68262b3360f74d7f8
kadufreitas <kadufreitas95@gmail.com>
2019-11-28 03:37:39 -0300

    tirando django_extensios

--- main/settings.py
+++ main/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-extensions',
     'django.contrib.sites',
     'rest_framework',
     'drf_yasg',
